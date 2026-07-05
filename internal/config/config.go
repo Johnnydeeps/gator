1a6804eb9f44b5c00282ab10c867596f6721c17b
Jack Aldus <jack.aldus@gmail.com>
2026-07-05 20:50:58 +0100

    B: add config package for reading and writing gator config(json) file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,66 @@
-package main
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+const configFileName = ".gatorconfig.json"
+
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	filepath := filepath.Join(home, configFileName)
+	return filepath, nil
+}
+
+func Read() (Config, error) {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
+	}
+	return cfg, nil
+}
+
+func write(cfg Config) error {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(cfg)
+}
+
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(*cfg)
+}
--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Johnnydeeps/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cfg.SetUser("jack")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cfg)
+}
